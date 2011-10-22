a923708f7adbadce8195fc4668a50156b519d026
Mozafar.Haider <kabaros@gmail.com>
2011-10-23 00:03:47 +0100

    commiting, again

---
+++ Exceptions/UnsuportedOrientationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MartianRobots.Exceptions
+{
+    public class UnsuportedOrientation : MartianException
+    {
+        public UnsuportedOrientation()
+            : base("Orientations should be N,W,E or S")
+        {
+        }
+    }
+}
---
+++ IParser/IParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MartianRobots.ProblemDescription;
+
+namespace MartianRobots.Parsers
+{
+    public interface IParser
+    {
+        PlanetDescription Parse(string commands);
+    }
+}
